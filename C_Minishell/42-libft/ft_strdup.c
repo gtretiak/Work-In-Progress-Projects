63649a97c1c34a5ed9afc150d3604d39c0ae0d1f
Georgii Tretiakov <george22021988@gmail.com>
2025-05-21 18:25:18 +0100

    Last update

--- C_Minishell/42-libft/ft_strdup.c
+++ C_Minishell/42-libft/ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gtretiak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: husamuel <husamuel@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 19:02:26 by gtretiak          #+#    #+#             */
-/*   Updated: 2025/05/14 13:31:51 by gtretiak         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:21:34 by husamuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 char	*ft_strdup(char *src)
 {
-	int		i;
 	char	*res;
+	int		i;
 
 	i = 0;
+	if (!src)
+		return (NULL);
 	while (src[i])
 		i++;
-	res = (char *)malloc((sizeof(*res) * i) + 1);
-	if (res == NULL)
+	res = (char *)malloc(sizeof(char) * (i + 1));
+	if (!res)
 		return (NULL);
 	i = 0;
 	while (src[i])
